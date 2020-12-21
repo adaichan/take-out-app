1c8146825dd4941003f6f714e3a2653712a6a2f3
ADAI是也！ <63220078+adaichan@users.noreply.github.com>
2020-12-21 13:18:30 +0800

    Add files via upload

---
+++ src/components/CartControl/CartControl.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="cartcontrol">
+    <transition name="move">
+      <!-- 加stop停止点击时子元素事件冒泡 -->
+      <div
+        class="iconfont icon-remove_circle_outline"
+        v-if="food.count"
+        @click.stop="updateFoodCount(false)"
+      ></div>
+    </transition>
+    <div class="cart-count" v-if="food.count">{{food.count}}</div>
+    <div class="iconfont icon-add_circle" @click.stop="updateFoodCount(true)"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    food: Object
+  },
+
+  methods: {
+    //  更新食物数量:是否增加
+    updateFoodCount(isAdd) {
+      //  更新数据的行为不写在一般组件里,通知vuex管理去触发action调用
+      //  传递多个参数用对象括起来
+      this.$store.dispatch('updateFoodCount', {isAdd, food: this.food})
+    }
+  },
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import '../../common/stylus/mixins.styl';
+
+.cartcontrol {
+  font-size: 0;
+
+  .cart-decrease {
+    display: inline-block;
+    padding: 6px;
+    line-height: 24px;
+    font-size: 24px;
+    color: rgb(0, 160, 220);
+  }
+
+  .icon-remove_circle_outline {
+    display: inline-block;
+    padding: 6px;
+    line-height: 24px;
+    font-size: 24px;
+    color: $green;
+
+    &.move-enter-active, &.move-leave-active {
+      transition: all 0.3s;
+    }
+
+    &.move-enter, &.move-leave-to {
+      opacity: 0;
+      transform: translateX(15px) rotate(180deg);
+    }
+  }
+
+  .cart-count {
+    display: inline-block;
+    vertical-align: top;
+    width: 12px;
+    padding-top: 6px;
+    line-height: 24px;
+    text-align: center;
+    font-size: 10px;
+    color: rgb(147, 153, 159);
+  }
+
+  .icon-add_circle {
+    display: inline-block;
+    padding: 6px;
+    line-height: 24px;
+    font-size: 24px;
+    color: $green;
+  }
+}
+</style>
